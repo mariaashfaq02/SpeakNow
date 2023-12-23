a90e3efd66bc28159db5b2c4617aac7fc812f7eb
mariaashfaq02 <mariaashfaq.02@gmail.com>
2023-12-24 02:02:13 +0500

    added css to contact  page

--- OneDrive/Desktop/SpeakNow/speak-now/src/Pages/Contact Page/Contact.js
+++ OneDrive/Desktop/SpeakNow/speak-now/src/Pages/Contact Page/Contact.js
@@ -2,32 +2,37 @@ import React from 'react'
 import NavBar from '../../Components/NavBar'
 import Accordion from '../../Components/Accordion/Accordion'
 import ContactForm from '../../Components/ContactForm'
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <div className='container-fluid'>
+    <div className='container-fluid contactPageContainer'>
       <NavBar/>
       
+      <div className="container-fluid contactPageMainContainer">
+        
+        <div className="container-fluid contactPageSubContainer">
+          <div className="container-fluid contactPageHeading">
+            <h1 className='display-1 contactMainHeading'>We don't bite</h1>
+            <h1 className='display-6 contactSubHeading'>Let's have a chat</h1>
+          </div>
 
-      <div className="container-fluid">
-        <h1 className='display-1'>We don't bite</h1>
-        <h1 className='display-6'>Let's have a chat</h1>
+          <div className="container-fluid contactFAQ">
+            <Accordion/>
+          </div>
+
+        </div>
+
+        <div className="container-fluid contactPageSubContainer">
+          <div className="container-fluid contactForm">
+            <ContactForm/>
+          </div>
+        </div>
+        
       </div>
 
-      <div className="container-fluid">
-        <Accordion/>
-      </div>
-
-      <div className="container-fluid">
-        <ContactForm/>
-      </div>
     </div>
-
-    
-
-    
-
-    
+  
   )
 }
 
