74aade2a6e54befcc5f66451277db3f49a5c6940
mariaashfaq02 <mariaashfaq.02@gmail.com>
2023-12-24 01:45:18 +0500

    added css to team page

--- OneDrive/Desktop/SpeakNow/speak-now/src/Pages/Team Page/Team.js
+++ OneDrive/Desktop/SpeakNow/speak-now/src/Pages/Team Page/Team.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import NavBar from '../../Components/NavBar'
 import { pictures } from './TeamData'
+import './Team.css'
 
 const Team = () => {
 
   const teamMembers=pictures.map(data=>{
     return(
-      <div className="container-fluid">
-        <img style={{  height: 500 }} src={data.image} className="img-fluid" alt={data.name} />
-        <p className='h6'>{data.name}</p>
+      <div className="container-fluid teamCard">
+        <img  src={data.image} className="img-fluid teamMemberPic" alt={data.name} />
+        <p className='h6 teamMemberName'>{data.name}</p>
       </div>
     )
   })
@@ -18,10 +19,12 @@ const Team = () => {
     <div className='container-fluid'>
       <NavBar/>
       <div className="container-fluid">
-        <h1 className='display-1'>Meet the Team</h1>
+        <h1 className='display-1 teamHeading'>Meet the Team</h1>
       </div>
+
+      <div className="container-fluid teamCardsContainer">
       {teamMembers}
-      
+      </div>      
     </div>
   )
 }
