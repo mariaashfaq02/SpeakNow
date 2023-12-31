ac31f8abecf41a919f721041551521f89496d30c
mariaashfaq02 <mariaashfaq.02@gmail.com>
2023-12-31 13:45:38 +0500

    recording functionality added

--- OneDrive/Desktop/SpeakNow/speak-now/src/App.js
+++ OneDrive/Desktop/SpeakNow/speak-now/src/App.js
@@ -7,6 +7,7 @@ import Features from './Pages/Features Page/Features';
 import Team from './Pages/Team Page/Team';
 import Contact from './Pages/Contact Page/Contact';
 import ErrorPage from './Pages/Error Page/ErrorPage';
+import Record from './Pages/Record Page/Record';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path='/team' element={<Team/>}/>
           <Route path='/contact' element={<Contact/>}/>
           <Route path='/error' element={<ErrorPage/>}/>
+          <Route path='/record' element={<Record/>}/>
         </Routes>
       </Router>
       
