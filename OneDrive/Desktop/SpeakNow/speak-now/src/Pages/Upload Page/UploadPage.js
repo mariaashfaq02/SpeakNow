f625e7079042d63df686864390a20b6b641a7b27
mariaashfaq02 <mariaashfaq.02@gmail.com>
2024-01-13 23:39:23 +0500

    added more css to upload page

--- OneDrive/Desktop/SpeakNow/speak-now/src/Pages/Upload Page/UploadPage.js
+++ OneDrive/Desktop/SpeakNow/speak-now/src/Pages/Upload Page/UploadPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Uploader from '../../Components/Uploader/Uploader'
+import Button from '../../Components/Button/Button'
 import './UploadPage.css'
+
 const UploadPage = () => {
   return (
     <div className='container-fluid uploader'>
@@ -8,6 +10,7 @@ const UploadPage = () => {
             <h1 className='display-1 uploadVideoHeading'>Upload your video to process!</h1>
         </div>
         <Uploader/>
+        <div className="container-fluid featureButton"><Button message={"Upload"} link={"feedback"}></Button></div>
     </div>
   )
 }
